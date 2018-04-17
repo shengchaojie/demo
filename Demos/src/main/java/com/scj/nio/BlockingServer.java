aae8b765bf08a59e4adea26679f679eb5a0ad5c3
shengchaojie <chaojiesheng@alog.com>
2018-04-17 19:36:14 +0800

    有毒的io

--- Demos/src/main/java/com/scj/nio/BlockingServer.java
+++ Demos/src/main/java/com/scj/nio/BlockingServer.java
@@ -1,5 +1,6 @@
 package com.scj.nio;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -13,11 +14,15 @@ import java.nio.channels.SocketChannel;
  */
 public class BlockingServer {
 
+    public static final byte[] ENTER = new byte[]{13,10};
+
     public static void main(String[] args) throws IOException {
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         serverSocketChannel.socket() .bind(new InetSocketAddress(9999));
         while(true){
             final SocketChannel socketChannel = serverSocketChannel.accept();
+            socketChannel.configureBlocking(true);
+            socketChannel.write(ByteBuffer.wrap("\r\n".getBytes()));
             new Thread(new SocketThread(socketChannel)).start();
         }
     }
@@ -30,15 +35,28 @@ public class BlockingServer {
             this.socketChannel = socketChannel;
         }
 
-
         @Override
         public void run() {
             try {
+                ByteBuffer byteBuffer = ByteBuffer.allocate(200);
+                byteBuffer.put((byte)1);
+                byteBuffer.get();
+                StringBuilder sb = new StringBuilder();
                 while (true){
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(200);
                     socketChannel.read(byteBuffer);
                     byteBuffer.flip();
-                    socketChannel.write(byteBuffer);
+                    if(byteBuffer.limit()>1&&byteBuffer.get()==ENTER[0]&&byteBuffer.get()==ENTER[1]){
+                        socketChannel.write(ByteBuffer.wrap(sb.toString().getBytes()));
+                        socketChannel.write(ByteBuffer.wrap("\r\n".getBytes()));
+                        byteBuffer.clear();
+                        sb = new StringBuilder();
+                        continue;
+                    }
+                    while (byteBuffer.hasRemaining()){
+                        sb.append((char)byteBuffer.get());
+                    }
+                    //必须进行clear,不然阻塞不住
+                    //byteBuffer.clear();
                 }
             }catch (Exception ex){
 
