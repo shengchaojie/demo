6ab502c8967237a80a73e1a75817c016effd7009
shengchaojie <chaojiesheng@alog.com>
2018-04-23 11:46:33 +0800

    server修改

--- Demos/src/main/java/com/scj/nio/NettyServer.java
+++ Demos/src/main/java/com/scj/nio/NettyServer.java
@@ -5,16 +5,16 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.util.CharsetUtil;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.charset.Charset;
 
 /**
@@ -23,6 +23,7 @@ import java.nio.charset.Charset;
 public class NettyServer {
 
     public static void main(String[] args) throws InterruptedException {
+        //两个线程池，对应IO线程池，业务线程池
         NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup();
         NioEventLoopGroup childEventLoopGroup = new NioEventLoopGroup();
         ServerBootstrap bootstrap = new ServerBootstrap();
@@ -33,24 +34,166 @@ public class NettyServer {
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel socketChannel) throws Exception {
-                        socketChannel.pipeline().addLast(new SimpleChannelInboundHandler<ByteBuf>() {
-
-                            @Override
-                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
-                                System.out.println(ByteBufUtil.hexDump(byteBuf.readBytes(byteBuf.readableBytes())));
-                                channelHandlerContext.write(Unpooled.copiedBuffer("hello\r\n", CharsetUtil.UTF_8));
-                            }
-
-                            @Override
-                            public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-                                ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);
-                            }
-                        });
+                        socketChannel.pipeline()
+                                .addLast(new TestInboundChannelHandler("first in "))
+                                .addLast(new TestInboundChannelHandler("second in "))
+                                .addLast(new EchoChannelHandler())
+                                .addLast(new TestOutboundChannelHandler("first out"))
+                                .addLast(new TestOutboundChannelHandler("second out"));
 
                     }
                 });
+        //等到绑定完成
         ChannelFuture channelFuture = bootstrap.bind().sync();
+        //等到serversocketchannel close在退出
         channelFuture.channel().closeFuture().sync();
 
+    }
+
+    static class TestInboundChannelHandler extends ChannelInboundHandlerAdapter{
+
+        private String name;
+
+        public TestInboundChannelHandler(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(name+"channelRegistered spread");
+            ctx.fireChannelRegistered();
+        }
+
+        @Override
+        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(name+"channelUnregistered not spread");
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(name+"channelActive not spread");
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(name+"channelInactive not spread");
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            System.out.println(name+"channelRead spread");
+            ctx.fireChannelRead(msg);
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(name+"channelReadComplete spread");
+            ctx.fireChannelReadComplete();
+        }
+    }
+
+    static class EchoChannelHandler extends ChannelInboundHandlerAdapter{
+        @Override
+        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("echo channelRegistered");
+            //ctx.writeAndFlush(Unpooled.copiedBuffer("Hello\r\n".getBytes()));
+        }
+
+        @Override
+        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("echo channelUnregistered");
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("echo channelInactive");
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("echo channelActive");
+            ctx.writeAndFlush(Unpooled.copiedBuffer("Hello\r\n".getBytes()));
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            System.out.println("echo channelRead");
+            ByteBuf byteBuf = (ByteBuf)msg;
+            System.out.println(new String(ByteBufUtil.getBytes(byteBuf)));
+            //ctx.write(msg);
+            ctx.channel().write(msg);
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("echo channelReadComplete");
+            ctx.channel().flush();
+        }
+    }
+
+    static class TestOutboundChannelHandler extends ChannelOutboundHandlerAdapter{
+
+        private String name;
+
+        public TestOutboundChannelHandler(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void bind(ChannelHandlerContext ctx, SocketAddress localAddress, ChannelPromise promise) throws Exception {
+            //super.bind(ctx,localAddress,promise);
+            System.out.println(name+ "bind");
+        }
+
+        @Override
+        public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) throws Exception {
+            super.connect(ctx,remoteAddress,localAddress,promise);
+            System.out.println(name+"connect");
+        }
+
+        @Override
+        public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+            //super.disconnect(ctx,promise);
+            System.out.println(name+"disconnect");
+        }
+
+        @Override
+        public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+            //super.close(ctx,promise);
+            System.out.println(name+"close");
+        }
+
+        @Override
+        public void deregister(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+            System.out.println(name+"deregister");
+        }
+
+        @Override
+        public void read(ChannelHandlerContext ctx) throws Exception {
+            ctx.read();
+            System.out.println(name+"read");
+        }
+
+        @Override
+        public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+            //super.write(ctx,msg,promise);
+            ctx.write(msg,promise);
+            System.out.println(name+"write");
+            //promise.setSuccess();
+            promise.addListener(new GenericFutureListener<Future<? super Void>>() {
+                @Override
+                public void operationComplete(Future<? super Void> future) throws Exception {
+                    if(future.isSuccess()){
+                        System.out.println(name+" listener trigger");
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void flush(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(name+"flush");
+            ctx.flush();
+        }
     }
 }
